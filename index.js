88a10ae3e6def3989730e370dd13937ec651c71e
skellofr <lucastwitch9@gmail.com>
2025-04-27 21:04:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const { status } = require('minecraft-server-util');
+require('dotenv').config();
 const config = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
